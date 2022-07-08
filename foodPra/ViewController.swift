7581e3ddd069a4a475fb0be638df8b9d864a7753
Daniel Kwon <skansmfqh7312@gmail.com>
2022-07-08 09:22:08 +0900

    Initial Commit

---
+++ foodPra/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  foodPra
+//
+//  Created by 권민서 on 2022/07/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+
+}
+
